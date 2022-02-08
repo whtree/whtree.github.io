3a2e82358649130483fc4518cb1a306d3dd93a8c
tree <w17596579@163.com>
2022-02-08 18:47:04 +0800

    game-demo

---
+++ game-demo/src/settings.37399.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Main.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"6fc01",main:"8dfb6"}};
